e11bb79eb9aa4a97be7652560f37ed2b310dac79
DEXO16\Mauro <dexa@hotmail.it>
2017-10-03 16:18:45 +0200

    ritornate tra noi

--- Schoolib/src/Pippo.java
+++ Schoolib/src/Pippo.java
@@ -1,4 +1,12 @@
 
 public class Pippo {
 // ciao 
-}       //jvjdvdvfdv
+// CIAO BY MAURO
+// ciao by mauro commit 11
+// ciao by mauro commit 12
+	
+// ciao committo sul 4	
+// ciao committo sul 5
+	
+	
+}//jvjdvdvfdv
